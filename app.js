aab1b937ae24810c63f2cf41c455cc8da49f9bab
zachary webb <zachsavn@gmail.com>
2022-06-28 11:18:40 -0400

    completed exercise 1

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arraySum (arr) {
+
+    let sum = 0;
+    
+    arr.forEach((num) => {
+     sum += num;
+     
+    });
+    return sum;
+}
+console.log(arraySum(numbers));
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
