c4f334eebf51fb6965f08ba34283f19ff83824dd
zachary webb <zachsavn@gmail.com>
2022-06-28 12:24:06 -0400

    Completed second exercise

--- app.js
+++ app.js
@@ -5,17 +5,25 @@ console.log("EXERCISE 1:\n==========\n");
 
 const numbers = [2, 22, 12, 17, 18, 39, 129];
 
-function arraySum (arr) {
+function arraySum(arr) {
+  let sum = 0;
 
-    let sum = 0;
-    
-    arr.forEach((num) => {
-     sum += num;
-     
-    });
-    return sum;
+  arr.forEach((num) => {
+    sum += num;
+  });
+
+  return sum;
 }
 console.log(arraySum(numbers));
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+let book = {};
+book.title = "Name of the Wind by Patrick Rothfuss";
+book.pages = 662;
+book.readCount = 5;
+book.info = () => {
+  return `${book.title}, ${book.pages} pages, read ${book.readCount} times.`;
+};
+
+console.log(book.info());
